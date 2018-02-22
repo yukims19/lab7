a26e43b2396451172fcfb5c5a1cdfbacc70926df
Youxi Li <yol107@ucsd.edu>
2018-02-21 22:47:09 -0800

    lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	  // your code here
+    $('.likeBtn').click(function(e){
+        ga("send", "event", 'like', 'click');
+
+    })
 }
